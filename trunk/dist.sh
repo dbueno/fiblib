5bfd59f9537c195d710577c400f635f252e7d2d7
dbueno <dbueno@069044af-cd31-0410-a904-216131717e12>
2007-06-02 15:28:52 +0000

    exclude files that shouldn't be in the distribution.

--- trunk/dist.sh
+++ trunk/dist.sh
@@ -2,7 +2,10 @@
 
 FIBLIB_VERSION=`cat VERSION`
 
-echo Creating distribution "$FIBLIB_VERSION..."
+TAR_FILENAME=fiblib-${FIBLIB_VERSION}.tar.gz
 
-tar cvzf fiblib-${FIBLIB_VERSION}.tar.gz .
+echo Creating distribution "$TAR_FILENAME..."
+
+tar czf $TAR_FILENAME . --exclude "${TAR_FILENAME}" \
+  --exclude ".svn" --exclude "._bcdi" --exclude "._d" --exclude "._ncdi"
 
