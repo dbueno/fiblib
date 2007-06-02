79af3eb0e3f07f38a38819ae1d5159eaf16aefa2
dbueno <dbueno@069044af-cd31-0410-a904-216131717e12>
2007-06-02 21:37:28 +0000

    More interface draft.

--- src/heap.mli
+++ src/heap.mli
@@ -5,19 +5,28 @@ type 'a node
 exception Empty
 exception UnsupportedOperation
 
-val create : unit -> 'a heap
+val create : int -> 'a heap
 
-val insert : 'a heap -> 'a node -> unit
+val add : 'a heap -> 'a node -> unit
 
-val extract_min : 'a heap -> 'a node
+val pop_min : 'a heap -> 'a node
+
+val peek_min : 'a heap -> 'a node
+
+val delete : 'a heap -> 'a node -> unit
 
 val decrease_key : 'a heap -> 'a node -> key -> unit
+
+val is_empty : 'a heap -> bool
 
 val size : 'a heap -> int
 
 
 val node_new : key:key -> data:'a -> 'a node
+
 val node_data : 'a node -> 'a
+
 val node_key : 'a node -> key
+
 
 val print : ('a -> string) -> Format.formatter -> 'a heap -> unit
