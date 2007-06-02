46b8256148b1ad5f2cb6701afcd4b68aaa08bbb3
dbueno <dbueno@069044af-cd31-0410-a904-216131717e12>
2007-06-02 15:54:43 +0000

    Added note about complexity of operations.

--- trunk/src/fibHeap.mli
+++ trunk/src/fibHeap.mli
@@ -3,6 +3,12 @@
 
     {3 Fiblib }
 
+    Fibonacci heaps are kind of an obscure data structure, but they have their
+    purposes. Operations that do not delete an element run in [O(1)] amortised
+    time. In particular, if the number of [extract-min] and [delete] operations is
+    small compared to the number of other operations, fibonacci heaps are a win
+    (CLRS 477).
+
     In this library, each heap is conceptually a min-heap (elements are closer
     to the root whose keys are less than other keys). However, the comparison
     function is parameterised by functorisation, so it is just as easy to create a
