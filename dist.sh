597f2171ac41eaec8856663f989660867fd70e5b
dbueno <dbueno@069044af-cd31-0410-a904-216131717e12>
2007-07-20 13:56:03 +0000

    exclude dist.sh and .depend

--- dist.sh
+++ dist.sh
@@ -7,5 +7,6 @@ TAR_FILENAME=fiblib-${FIBLIB_VERSION}.tar.gz
 echo Creating distribution "$TAR_FILENAME..."
 
 tar czf $TAR_FILENAME . --exclude "${TAR_FILENAME}" \
-  --exclude ".svn" --exclude "._bcdi" --exclude "._d" --exclude "._ncdi"
+  --exclude ".svn" --exclude "._bcdi" --exclude "._d" --exclude "._ncdi" \
+  --exclude "dist.sh" --exclude ".depend"
 
